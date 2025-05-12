4d358f0883db5425f06466c5fbce47b1f8b17214
NikhilKumar7824 <116564547+NikhilKumar7824@users.noreply.github.com>
2025-05-12 14:01:35 +0530

    Update HelloWorldController.java

--- src/main/java/com/demo/controller/HelloWorldController.java
+++ src/main/java/com/demo/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 	
 	@GetMapping("/message")
 	public String getMessage() {
-		return "Hey ! this is Nikhil from Bhilai";
+		return "Hey ! this is Nikhil from rjn";
 	}
 }
